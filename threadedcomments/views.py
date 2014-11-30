09d19b4f5fd97850337fef1dd5092c09da423d0e
Kyle Gibson <kyle.gibson@frozenonline.com>
2014-11-30 16:21:40 -0500

    message_set was deprecated and removed in 1.4. Use new message framework

--- threadedcomments/views.py
+++ threadedcomments/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect, Http404, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext, Context, Template
 from django.utils.http import urlquote
@@ -102,7 +103,10 @@ def free_comment(request, content_type=None, object_id=None, edit_id=None, paren
         new_comment.save()
         if model == ThreadedComment:
             if add_messages:
-                request.user.message_set.create(message="Your message has been posted successfully.")
+                messages.success(
+                    request,
+                    message='Your message has been posted successfully',
+                )
         else:
             request.session['successful_data'] = {
                 'name' : form.cleaned_data['name'],
