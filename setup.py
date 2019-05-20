08fc3fa3c1fa300c8c95e9d8e0250f6622bf9437
Jason Ward <jason.ward@policystat.com>
2019-05-20 12:08:49 -0400

    bumped to version 0.5.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-threadedcomments',
-    version='0.5.4',
+    version='0.5.5',
     description='A simple yet flexible threaded commenting system.',
     author='Eric Florenzano',
     author_email='floguy@gmail.com',
